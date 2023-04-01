34f472afd9e4c28303831ddab44b11966029463b
romainvairo <romain.vairo@hotmail.com>
2023-04-01 11:00:45 +0200

    Deployment

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -125,4 +125,4 @@ def predict_sentiment(text: str):
 
 # Démarrage de l'API, elle démarrera sur cette adresse http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
